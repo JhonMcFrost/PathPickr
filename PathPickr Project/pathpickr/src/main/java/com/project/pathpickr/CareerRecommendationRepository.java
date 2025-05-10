c9582df9a962f225933e53f636a906e1deacd0c5
JhonMcFrost <jhonmcneil21@gmail.com>
2025-05-10 20:54:58 +0800

    Delete PathPickr Project/pathpickr directory

--- PathPickr Project/pathpickr/src/main/java/com/project/pathpickr/CareerRecommendationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.project.pathpickr;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CareerRecommendationRepository extends JpaRepository<CareerRecommendation, Long> {
-    CareerRecommendation findByTag(String tag);
-}
